41f921d253def83531a47ea544de7f9858a28ddf
Zxrsheep <1528701321@qq.com>
2020-12-31 12:21:49 +0800

    修改了feign和网关集成后第一次调用必失败的问题。

--- match-music/music-api-gateway-zuul/src/main/java/com/music_zuul/filter/FilterProperties.java
+++ match-music/music-api-gateway-zuul/src/main/java/com/music_zuul/filter/FilterProperties.java
@@ -1,21 +1,21 @@
-package com.music_zuul.filter;
-
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.util.List;
-
-@ConfigurationProperties(prefix = "leyou.filter")
-public class FilterProperties {
-
-    private List<String> allowPaths;
-
-    public List<String> getAllowPaths() {
-        return allowPaths;
-    }
-
-    public void setAllowPaths(List<String> allowPaths) {
-        this.allowPaths = allowPaths;
-    }
-
-}
+//package com.music_zuul.filter;
+//
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+//import java.util.List;
+////leyou.filter.allowPaths=/api-user
+//@ConfigurationProperties(prefix = "leyou.filter")
+//public class FilterProperties {
+//
+//    private List<String> allowPaths;
+//
+//    public List<String> getAllowPaths() {
+//        return allowPaths;
+//    }
+//
+//    public void setAllowPaths(List<String> allowPaths) {
+//        this.allowPaths = allowPaths;
+//    }
+//
+//}
